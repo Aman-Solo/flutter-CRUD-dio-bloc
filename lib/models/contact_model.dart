17e731449e853c34977d974ddcb6bc6221d07fbf
Amanuel Solomon <amanuel.ugr-0540-16@aau.edu.et>
2026-05-17 19:38:32 +0300

    Initial commit

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+class Contact extends Equatable {
+  final int? id;
+  final String name;
+  final String email;
+  final String phone;
+
+  const Contact({
+    this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+  });
+  factory Contact.fromJson(Map<String, dynamic> json){
+    return Contact(
+      id: json['id'] as int?,
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phone: json['phone'] ?? '',
+    );
+  }
+  Map<String, dynamic> toJson(){
+    return{
+      if (id != null) 'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+    };
+  }
+  Contact copyWith({
+    int? id,
+    String? name,
+    String? email,
+    String? phone,
+  }) {
+    return Contact(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phone: phone ?? this.phone,
+    );
+  }
+  @override
+  List<Object?> get props => [id, name, email, phone];
+  }
